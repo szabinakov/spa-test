7d110be6db81be6d4b7a73cc6cb6fe3b399be48a
szabinakov <szabinkov@gmail.com>
2020-12-18 12:38:23 +0000

    styled Register component

--- src/components/Register.js
+++ src/components/Register.js
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { Link, useHistory } from "react-router-dom";
 import axios from "axios";
 import PropTypes from "prop-types";
+import "../styles/Register.css";
 
 export default function Register({ userState, setUserState }) {
   const [value, setValue] = useState();
@@ -35,40 +36,54 @@ export default function Register({ userState, setUserState }) {
     }
   };
   return (
-    <form onSubmit={handleSubmit}>
-      <label htmlFor="email">Email</label>
-      <input
-        type="text"
-        id="email"
-        className="email"
-        name="email"
-        placeholder="Please enter your email address!"
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="password">Password</label>
-      <input
-        type="password"
-        id="password"
-        className="password"
-        name="password"
-        placeholder="Please enter your chosen password!"
-        onChange={handleChange}
-        required
-      />
-      <label htmlFor="confirmPassword">Confirm Password</label>
-      <input
-        type="password"
-        id="confirmPassword"
-        className="confirmPassword"
-        name="confirmPassword"
-        placeholder="Please confirm your password!"
-        onChange={handleChange}
-        required
-      />
-      <button type="submit">Register</button>
-      <Link to="/">Already member? Login here!</Link>
-    </form>
+    <div className="registerFormContainer">
+      <form className="registerForm" onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Email</label>
+          <input
+            type="text"
+            id="email"
+            className="email"
+            name="email"
+            placeholder="Please enter your email address!"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            id="password"
+            className="password"
+            name="password"
+            placeholder="Please enter your chosen password!"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            id="confirmPassword"
+            className="confirmPassword"
+            name="confirmPassword"
+            placeholder="Please confirm your password!"
+            onChange={handleChange}
+            required
+          />
+        </div>
+        <div>
+          <button type="submit">Register</button>
+        </div>
+        <div>
+          <Link to="/">
+            <p>Already member? Login here!</p>
+          </Link>
+        </div>
+      </form>
+    </div>
   );
 }
 
