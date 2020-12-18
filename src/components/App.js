1e4da92b4db7359cefd9e5a0a293d977f026a39d
szabinakov <szabinkov@gmail.com>
2020-12-18 11:31:20 +0000

    added Redirect to AddUser route, now only accessible when logged in

--- src/components/App.js
+++ src/components/App.js
@@ -2,7 +2,9 @@ import "../styles/App.css";
 import { useState } from "react";
 import Home from "./Home";
 import Register from "./Register";
-import { Route } from "react-router-dom";
+import Navbar from "./Navbar.js";
+import AddUsers from "./AddUsers.js";
+import { Route, Switch, Redirect } from "react-router-dom";
 
 function App() {
   const initialState = {
@@ -14,20 +16,30 @@ function App() {
 
   return (
     <div className="App">
-      <Route
-        exact
-        path="/"
-        render={(props) => (
-          <Home {...props} userState={user} setUserState={setUser} />
-        )}
-      />
-      <Route
-        exact
-        path="/register"
-        render={(props) => (
-          <Register {...props} userState={user} setUserState={setUser} />
-        )}
-      />
+      <Navbar userState={user} />
+      <Switch>
+        <Route
+          exact
+          path="/"
+          render={(props) => (
+            <Home {...props} userState={user} setUserState={setUser} />
+          )}
+        />
+        <Route
+          exact
+          path="/register"
+          render={(props) => (
+            <Register {...props} userState={user} setUserState={setUser} />
+          )}
+        />
+        <Route
+          exact
+          path="/add-users"
+          render={() =>
+            user.isUserLoggedIn ? <AddUsers /> : <Redirect to="/" />
+          }
+        />
+      </Switch>
     </div>
   );
 }
