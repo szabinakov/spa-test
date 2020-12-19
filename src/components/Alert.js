6020a41b322a27f75a118b8b71b04f6fd0d22493
szabinakov <szabinkov@gmail.com>
2020-12-19 16:50:26 +0000

    styled Alert component

--- src/components/Alert.js
+++ src/components/Alert.js
@@ -1,10 +1,13 @@
 import React from "react";
 import PropTypes from "prop-types";
+import "../styles/Alert.css";
 
 export default function Alert({ message, success }) {
   if (!message) return null;
   return (
-    <div className={`alert-${success ? "success" : "error"}`}>{message}</div>
+    <div className="alertContainer">
+      <div className={`alert-${success ? "success" : "error"}`}>{message}</div>
+    </div>
   );
 }
 
