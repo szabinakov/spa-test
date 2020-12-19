984200bcd7d74dd266c04bdff212cb8c50d3ba54
szabinakov <szabinkov@gmail.com>
2020-12-19 18:32:10 +0000

    changed darkblue to darker color, styled Navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,27 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import PropTypes from "prop-types";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faUserPlus, faHome } from "@fortawesome/free-solid-svg-icons";
+import "../styles/Navbar.css";
 
 export default function Navbar({ userState }) {
   const isUserLoggedIn = userState.isUserLoggedIn;
 
   return (
-    <div>
+    <div className="navContainer">
       {isUserLoggedIn && (
         <ul>
-          <Link to="/">
-            <li>Home</li>
-          </Link>
-          <Link to="/add-users">
-            <li>Add Users</li>
-          </Link>
+          <li className="homeLink">
+            <Link to="/">
+              <FontAwesomeIcon className="iconContainer" icon={faHome} />
+            </Link>
+          </li>
+          <li className="addUserLink">
+            <Link to="/add-users">
+              <FontAwesomeIcon className="iconContainer" icon={faUserPlus} />
+            </Link>
+          </li>
         </ul>
       )}
     </div>
