921f78f4b700785e8e746c5b6d6a55e90f030c5e
szabinakov <szabinkov@gmail.com>
2020-12-19 00:49:41 +0000

    added variables for color because started to get out of hand :D:DD

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,21 +1,50 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
 import { faTrash } from "@fortawesome/free-solid-svg-icons";
+import {
+  faTwitter,
+  faInstagram,
+  faCodepen,
+  faLinkedin,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function UserCard({ id, name, email, avatar, deleteUser }) {
   return (
     <div className="userCardContainer">
+      <button onClick={() => deleteUser(id)}>
+        <span class="tooltiptext">Delete User</span>
+        <FontAwesomeIcon className="iconContainer" icon={faTrash} />
+      </button>
       <div className="imgContainer">
         <img src={avatar} alt="face of people" />
       </div>
       <p className="nameContainer">{name}</p>
       <p className="emailContainer">{email}</p>
-      <div className="socialContainer">
-        <button onClick={() => deleteUser(id)}>
-          <span class="tooltiptext">Delete User</span>
-          <FontAwesomeIcon className="iconContainer" icon={faTrash} />
-        </button>
+      <div className="socialDiv">
+        <ul className="socialIcons">
+          <li>
+            <a href="https://www.instagram.com/">
+              <FontAwesomeIcon className="iconContainer" icon={faInstagram} />
+            </a>
+          </li>
+          <li>
+            <a href="https://twitter.com/?lang=en-gb">
+              <FontAwesomeIcon className="iconContainer" icon={faTwitter} />
+            </a>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/feed/">
+              <FontAwesomeIcon className="iconContainer" icon={faLinkedin} />
+            </a>
+          </li>
+          <li>
+            <a href="https://codepen.io/">
+              <FontAwesomeIcon className="iconContainer" icon={faCodepen} />
+            </a>
+          </li>
+        </ul>
       </div>
     </div>
   );
